d8a89a1f84a4c95766b49db913ff54eb9c3d7627
meeshyd <michellemdidier@gmail.com>
2017-04-10 20:52:28 -0500

    removed unwanted fil

--- keys.js
+++
@@ -1,6 +0,0 @@
-exports.twitterKeys = {
-  consumer_key: '0bWTqBWMTkxRhuTIMiZ9gt0Wu',
-  consumer_secret: 'OImxat4wJeVbWDySxCQGvQqHMK8Hjv78ZvyJ5Ll1oGbyD4YIkd',
-  access_token_key: '2586679800-Kg8W7iFSsYbhzqvq7alZYrD4VR5bQmH1GhRBiEO',
-  access_token_secret: '32wICEwcEJeD4hJQKLFLDpv5KPcktZVadsJUdywDrU0AN',
-}
